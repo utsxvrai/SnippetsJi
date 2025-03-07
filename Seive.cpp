e1099069a5ad5823a0da2e62c27fc836ec1fda78
Utsav Rai <utsavrai2115@gmail.com>
2025-03-07 09:39:23 +0530

    seive

---
+++ Seive.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
+// Function to generate all prime numbers up to n using the Sieve of Eratosthenes
+vector<int> sieveOfEratosthenes(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false; // 0 and 1 are not prime numbers
+
+    for (int p = 2; p * p <= n; ++p) {
+        if (isPrime[p]) {
+            for (int i = p * p; i <= n; i += p) {
+                isPrime[i] = false;
+            }
+        }
+    }
+
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
+
+// Example usage
+int main() {
+    int n;
+    cout << "Enter the limit to find all prime numbers up to that number: ";
+    cin >> n;
+
+    vector<int> primes = sieveOfEratosthenes(n);
+
+    cout << "Prime numbers up to " << n << " are: ";
+    for (int prime : primes) {
+        cout << prime << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
